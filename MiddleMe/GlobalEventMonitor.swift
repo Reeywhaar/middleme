21f7b7303e97e5278b3eb3eda7440caff0ac4f3e
Vyrtsev Mikhail <reeywhaar@gmail.com>
2023-09-10 23:00:18 +0300

    Put MultiTouchSupport wrapper right into the project

--- MiddleMe/GlobalEventMonitor.swift
+++ MiddleMe/GlobalEventMonitor.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import SwiftUI
-import OpenMultitouchSupport
 import Combine
 
 class GlobalEventMonitor {
---
+++ MiddleMe/MiddleMe-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MiddleMe-Bridging-Header.h
+//  MiddleMe
+//
+//  Created by Mikhail Vyrtsev on 10.09.2023.
+//
+
+#ifndef MiddleMe_Bridging_Header_h
+#define MiddleMe_Bridging_Header_h
+
+#import "OpenMultiTouchSupport.h"
+
+#endif /* MiddleMe_Bridging_Header_h */
